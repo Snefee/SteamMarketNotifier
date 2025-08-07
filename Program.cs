db24a158100245ce81779ada82f2f86a70347a31
Snefee <fil.chol03@gmail.com>
2025-08-07 04:37:41 +0200

    Added a feature enabling the user to set what item they want to be tracked and set their Ntfy token ID for notifications. Implemented a configuration saving and loading feature. On the first launch the user is prompted to set the item, currency, price notification threshold and Ntfy token ID. After doing a first-time configuration, app loads the config.json file without the need to input everything each time the app is launched.
    TODO: Implement a feature so the user can change the configuration file directly from the app window and not by manually modifying the config.json as it is now.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.IO;
 
 
 public class SteamMarketPrice
@@ -22,15 +23,25 @@ public class SteamMarketPrice
     public string? MedianPrice { get; set; }
 }
 
+public class AppConfig
+{
+    public string ItemName { get; set; } = string.Empty;
+    public int CurrencyType { get; set; }
+    public string NtfyTopic { get; set; } = string.Empty;
+    public float PriceThreshold { get; set; }
+}
+
 public class Program
 {
     private static readonly HttpClient client = new HttpClient();
 
     private static int currencyType = 1; // Default currency = USD
-    private static string ApiUrl => $"https://steamcommunity.com/market/priceoverview/?currency={currencyType}&appid=730&market_hash_name=Stockholm%202021%20Legends%20Sticker%20Capsule";
+    private static string apiUrl => $"https://steamcommunity.com/market/priceoverview/?currency={currencyType}&appid=730&market_hash_name={itemName}";
 
-    private static float PriceThreshold;
-    private const string NtfyTopic = "x";
+    private static float priceThreshold;
+    private static string ntfyTopic = string.Empty;
+    private static bool isNtfyEnabled => !string.IsNullOrEmpty(ntfyTopic);
+    private static string itemName = string.Empty;
 
     // Last prices variables to track changes
     private static float? previousLowestPrice = null;
@@ -38,64 +49,146 @@ public class Program
 
     private static bool isAlertSent = false; // Prevents multiple alerts for the same price threshold
 
+    // File path for configuration
+    private static readonly string configFile = Path.Combine(AppContext.BaseDirectory, "config.json");
 
     // === Main method to configure all settings ===
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("--- Configure Price Alert ---");
-        while (true)
+        // Load configuration from file
+        var config = LoadConfig();
+        if (config != null)
         {
-            Console.Write("Input price threshold at which the alert is sent (ex. 08,50): ");
-            string? priceInput = Console.ReadLine();
+            itemName = config.ItemName;
+            currencyType = config.CurrencyType;
+            ntfyTopic = config.NtfyTopic;
+            priceThreshold = config.PriceThreshold;
 
-            // Convert comma to dot for decimal parsing
-            if (priceInput != null)
+            Console.WriteLine("--- Loaded configuration ---");
+            Console.WriteLine($"Tracked Item: {itemName}");
+            Console.WriteLine($"Currency Type: {currencyType}");
+            Console.WriteLine($"Ntfy Topic: {ntfyTopic}");
+            Console.WriteLine($"Price Threshold: {priceThreshold}");
+            Console.WriteLine("-----------------------------------");
+            Thread.Sleep(2500);
+        }
+        else
+        {
+            Console.WriteLine("--- Welcome to Steam Market Notifier! ---");
+            while (true)
             {
-                priceInput = priceInput.Replace(',', '.');
+                Console.WriteLine("Write the exact english name of the item you want to track (case-sensitive):");
+                string? itemInput = Console.ReadLine();
+
+
+                if (!string.IsNullOrEmpty(itemInput))
+                {
+                    itemName = itemInput.Replace(" ", "%20");
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Item name cannot be empty. Please try again.");
+                    Console.ResetColor();
+                }
             }
 
-            if (float.TryParse(priceInput, NumberStyles.Any, CultureInfo.InvariantCulture, out PriceThreshold) && PriceThreshold > 0)
+            Console.WriteLine("--- Choose your preffered currency ---");
+            while (true)
             {
-                break;
+                Console.WriteLine("Input currency type (1-10): ");
+                Console.WriteLine("1. USD (default)");
+                Console.WriteLine("2. GBP");
+                Console.WriteLine("3. EUR");
+                Console.WriteLine("4. CHF");
+                Console.WriteLine("5. RUB");
+                Console.WriteLine("6. PLN");
+                Console.WriteLine("7. BRL");
+                Console.WriteLine("8. JPY");
+                Console.WriteLine("9. NOK");
+                Console.WriteLine("10. AED");
+
+                string? currencyInput = Console.ReadLine();
+
+                if (int.TryParse(currencyInput, out currencyType) && currencyType >= 1 && currencyType <= 10)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong input. Input a number between 1 to 10.");
+                    Console.ResetColor();
+                }
             }
-            else
+
+            Console.WriteLine("--- Set your Ntfy token ID for notifications ---");
+            while (true)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wrong input. Input positive number.");
-                Console.ResetColor();
+                Console.WriteLine("If you don't have one, you can create it at https://ntfy.sh/");
+                Console.WriteLine("Or leave it empty and press enter to skip notifications.");
+
+                string? ntfyInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(ntfyInput))
+                {
+                    ntfyTopic = string.Empty; // No notifications
+                    break;
+                }
+                else if (ntfyInput.Length >= 5 && ntfyInput.Length <= 64)
+                {
+                    ntfyTopic = ntfyInput.Trim();
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong input. Input a valid token ID (5-64 characters).");
+                    Console.ResetColor();
+                }
             }
+
+            if (isNtfyEnabled == true)
+            {
+                Console.WriteLine("--- Configure Price Alert ---");
+                while (true)
+                {
+                    Console.Write("Input price threshold at which the alert is sent (ex. 08,50): ");
+                    string? priceInput = Console.ReadLine();
+
+                    // Convert comma to dot for decimal parsing
+                    if (priceInput != null)
+                    {
+                        priceInput = priceInput.Replace(',', '.');
+                    }
+
+                    if (float.TryParse(priceInput, NumberStyles.Any, CultureInfo.InvariantCulture, out priceThreshold) && priceThreshold > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong input. Input positive number.");
+                        Console.ResetColor();
+                    }
+                }
+            }
+
+                // Save configuration to file
+                config = new AppConfig
+                {
+                    ItemName = Uri.UnescapeDataString(itemName),
+                    CurrencyType = currencyType,
+                    PriceThreshold = priceThreshold,
+                    NtfyTopic = ntfyTopic
+                };
+            SaveConfig(config);
+
+            Console.WriteLine("Tracking Steam Market price of selected item...");
         }
 
-        Console.WriteLine("--- Choose your preffered currency ---");
-        while (true)
-        {
-            Console.WriteLine("Input currency type (1-10): ");
-            Console.WriteLine("1. USD (default)");
-            Console.WriteLine("2. GBP");
-            Console.WriteLine("3. EUR");
-            Console.WriteLine("4. CHF");
-            Console.WriteLine("5. RUB");
-            Console.WriteLine("6. PLN");
-            Console.WriteLine("7. BRL");
-            Console.WriteLine("8. JPY");
-            Console.WriteLine("9. NOK");
-            Console.WriteLine("10. AED");
-
-            string? currencyInput = Console.ReadLine();
-
-            if (int.TryParse(currencyInput, out currencyType) && currencyType >= 1 && currencyType <= 10)
-            {
-                break;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wrong input. Input a number between 1 to 10.");
-                Console.ResetColor();
-            }
-        }
-
-        Console.WriteLine("Tracking Steam Market price of selected item...");
         while (true)
         {
             await FetchAndDisplayPrice();
@@ -109,11 +202,12 @@ public class Program
     {
         Console.Clear();
         Console.WriteLine($"--- Last update: {DateTime.Now:HH:mm:ss} ---");
-        Console.WriteLine($"{ApiUrl}");
+        Console.WriteLine($"Tracking item: {itemName.Replace("%20", " ")}");
+        Console.WriteLine($"{apiUrl}");
 
         try
         {
-            string responseBody = await client.GetStringAsync(ApiUrl);
+            string responseBody = await client.GetStringAsync(apiUrl);
             var priceData = JsonSerializer.Deserialize<SteamMarketPrice>(responseBody);
 
             if (priceData?.Success == true && priceData.LowestPrice != null && priceData.MedianPrice != null)
@@ -130,7 +224,7 @@ public class Program
                 Console.WriteLine($"Median price change: \t{(previousMedianPrice.HasValue ? (currentMedian - previousMedianPrice.Value).ToString("F2", CultureInfo.InvariantCulture) : "N/A")}");
                 Console.WriteLine("------------------------------------------");
 
-                if (currentLowest > PriceThreshold && !isAlertSent)
+                if (currentLowest > priceThreshold && !isAlertSent && isNtfyEnabled == true)
                 {
                     Console.WriteLine("\nWent over the threshold! Sending notification...");
                     string message = $"Item's price went over the threshold! Current price: {priceData.LowestPrice}";
@@ -138,7 +232,7 @@ public class Program
                     isAlertSent = true; // Set alert as sent
                 }
                 // Reset alert if price falls below threshold
-                else if (currentLowest <= PriceThreshold && isAlertSent)
+                else if (currentLowest <= priceThreshold && isAlertSent)
                 {
                     Console.WriteLine("\nPrice fell below the threshold. Alert reset.");
                     isAlertSent = false;
@@ -150,7 +244,9 @@ public class Program
             }
             else
             {
-                Console.WriteLine("API did respond with a message, but with incorrect data");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("API did respond with a message, but with incorrect data. Check if item's name is correct");
+                Console.ResetColor();
             }
         }
         catch (Exception e)
@@ -167,7 +263,7 @@ public class Program
         try
         {
             var content = new StringContent(message, Encoding.UTF8, "text/plain");
-            var request = new HttpRequestMessage(HttpMethod.Post, $"https://ntfy.sh/{NtfyTopic}")
+            var request = new HttpRequestMessage(HttpMethod.Post, $"https://ntfy.sh/{ntfyTopic}")
             {
                 Content = content
             };
@@ -223,5 +319,36 @@ public class Program
         // If previousPrice is null, just print the price without color
         Console.WriteLine(priceString);
         Console.ResetColor();
+    }
+
+    private static AppConfig? LoadConfig()
+    {
+        if (File.Exists(configFile))
+        {
+            try
+            {
+                string json = File.ReadAllText(configFile);
+                return JsonSerializer.Deserialize<AppConfig>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading config file: {ex.Message}");
+                return null;
+            }
+        }
+        return null;
+    }
+
+    private static void SaveConfig(AppConfig config)
+    {
+        try
+        {
+            string json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(configFile, json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error saving config file: {ex.Message}");
+        }
     }
 }
